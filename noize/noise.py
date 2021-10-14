ee4de753029af6d58ad661032fa8a36654bba960
M.Cemil Guney <mcemilguneyy@gmail.com>
2021-10-14 23:22:04 +0300

    add docstrings

--- noize/noise.py
+++ noize/noise.py
@@ -5,6 +5,7 @@ from typing import Callable
 
 
 def __periodic_noise(im: np.ndarray, angle: int, wavelength: int) -> np.ndarray:
+    """Apply periodic noise to 2d shape np.ndarray."""
     x = np.arange(0, im.shape[0])
     y = np.arange(0, im.shape[1])
     xv, yv = np.meshgrid(x, y)
@@ -15,7 +16,35 @@ def __periodic_noise(im: np.ndarray, angle: int, wavelength: int) -> np.ndarray:
     return noise_im
 
 
-def periodic(image: np.ndarray, mode: str, angle: int, wavelength: int) -> np.ndarray:
+def periodic(image: np.ndarray, mode: str="gray", angle: int=0, wavelength: int=100) -> np.ndarray:
+    """Applies periodic noise to given image.
+
+    Parameters
+    ----------
+    image : np.ndarray
+        The image which the noise will be added. It can be gray or RGB image.
+    mode : str, optional
+        The mode defines noise channel to be applied. It can be "gray", "R", "G" or "+" which applies noise
+        to all RGB channels. All modes expect RGB images expect "gray" which can be used with both gray and
+        RGB images. If the mode is "gray" and image is RGB the output will be gray. (Default "gray")
+    angle : int, optional
+        The angle of the periodic noise. (Default 0).
+    wavelength : int, optional
+        The wavelength of the periodic (sinusoidal) noise. (Default 100).
+    
+    Raises
+    ------
+    noize.util.BadModeException
+        If the mode not given properly.
+    noize.util.BadShapeException
+        If the shape is not proper.
+    
+    Returns
+    -------
+    np.ndarray
+        The noise applied image. It will be in same shape with the input image unless the mode 
+        is "gray" and the given image is RGB.
+    """
     util.check_input(image, accepted_shapes=("gray", "RGB"))
     im_arr = image/255.0
     if mode == "gray":
@@ -43,8 +72,29 @@ def periodic(image: np.ndarray, mode: str, angle: int, wavelength: int) -> np.nd
     return (noise_im*255).astype(np.uint8)
 
 
-def salt_and_pepper(image: np.ndarray, prob: float, seed: int=None) -> np.ndarray:
-    """Apply salt and pepper noise to given grayscale or rgb image with given prob."""
+def salt_and_pepper(image: np.ndarray, prob: float=0.1, seed: int=None) -> np.ndarray:
+    """Apply salt and pepper noise to given grayscale or rgb image with given prob.
+    
+    Parameters
+    ---------- 
+    image : np.ndarray
+        The image which the noise will be added. It can be gray, RGB or with multiple channels.
+        The noise will be applied to all channels seperately.
+    prob : float, optional
+        The probablity that sp noise to apply. Default 0.1
+    seed : int, optional
+        Seed to be used while adding noise randomly. Default None.
+    
+    Raises
+    ------
+    noize.util.BadShapeException
+        If the shape is not proper.
+
+    Returns
+    -------
+    np.ndarray
+        The noise applied image. It will be in same shape with the input image.
+    """
     util.check_input(image)
     output = image.copy()
     rng = np.random.default_rng(seed)
@@ -63,28 +113,154 @@ def salt_and_pepper(image: np.ndarray, prob: float, seed: int=None) -> np.ndarra
     return output.astype(np.uint8)
 
 
-def gaussian(image: np.ndarray, mean: float, var: float, seed: int=None) -> np.ndarray:
+def gaussian(image: np.ndarray, mean: float=0.0, var: float=0.01, seed: int=None) -> np.ndarray:
+    """Apply gaussian noise to given grayscale or rgb image.
+
+    For the gaussian random generator numpy.random.random function used.
+
+    Parameters
+    ---------- 
+    image : np.ndarray
+        The image which the noise will be added. It can be gray, RGB or with multiple channels.
+    mean : float, optional
+        The mean of the distribution. Default 0.0
+    var : float, optional
+        The variance of the distribution. Default 0.01
+    seed : int, optional
+        Seed to be used while adding noise randomly. Default None.
+    
+    Raises
+    ------
+    noize.util.BadShapeException
+        If the shape is not proper.
+
+    Returns
+    -------
+    np.ndarray
+        The noise applied image. It will be in same shape with the input image.
+    """
     rng = np.random.default_rng(seed)
     return __noise_with_pdf(image, rng.normal, loc=mean, scale=var**0.5)
 
 
-def rayleigh(image: np.ndarray, loc: float, scale: float, seed: int=None) -> np.ndarray:
+def rayleigh(image: np.ndarray, loc: float=0.0, scale: float=0.1, seed: int=None) -> np.ndarray:
+    """Apply rayleigh noise to given grayscale or rgb image.
+
+    Check scipy.stats.rayleigh for more information.
+
+    Parameters
+    ---------- 
+    image : np.ndarray
+        The image which the noise will be added. It can be gray, RGB or with multiple channels.
+    loc : float, optional
+        Loc (center) of the distribution. Default 0.0
+    scale : float, optional
+        Scale of the distribution. Default 0.1
+    seed : int, optional
+        Seed to be used while adding noise randomly. Default None.
+    
+    Raises
+    ------
+    noize.util.BadShapeException
+        If the shape is not proper.
+
+    Returns
+    -------
+    np.ndarray
+        The noise applied image. It will be in same shape with the input image.
+    """
     return __noise_with_pdf(image, stats.rayleigh.rvs, loc=loc, scale=scale, random_state=seed)
 
 
 def erlang(image: np.ndarray, a: int, loc: float, scale: float, seed: int=None) -> np.ndarray:
+    """Apply erlang (gamma) noise to given grayscale or rgb image.
+
+    Check scipy.stats.gamma for more information.
+
+    Parameters
+    ---------- 
+    image : np.ndarray
+        The image which the noise will be added. It can be gray, RGB or with multiple channels.
+    loc : float, optional
+        Loc (center) of the distribution. Default 0.0
+    scale : float, optional
+        Scale of the distribution. Default 0.1
+    seed : int, optional
+        Seed to be used while adding noise randomly. Default None.
+    
+    Raises
+    ------
+    noize.util.BadShapeException
+        If the shape is not proper.
+
+    Returns
+    -------
+    np.ndarray
+        The noise applied image. It will be in same shape with the input image.
+    """
     return __noise_with_pdf(image, stats.gamma.rvs, a=a, loc=loc, scale=scale, random_state=seed)
 
 
 def exponential(image: np.ndarray, loc: float, scale: float, seed: int=None) -> np.ndarray:
+    """Apply exponential noise to given grayscale or rgb image.
+
+    Check scipy.stats.expon for more information.
+
+    Parameters
+    ---------- 
+    image : np.ndarray
+        The image which the noise will be added. It can be gray, RGB or with multiple channels.
+    loc : float, optional
+        Loc (center) of the distribution. Default 0.0
+    scale : float, optional
+        Scale of the distribution. Default 0.1
+    seed : int, optional
+        Seed to be used while adding noise randomly. Default None.
+    
+    Raises
+    ------
+    noize.util.BadShapeException
+        If the shape is not proper.
+
+    Returns
+    -------
+    np.ndarray
+        The noise applied image. It will be in same shape with the input image.
+    """
     return __noise_with_pdf(image, stats.expon.rvs, loc=loc, scale=scale, random_state=seed)
 
 
 def uniform(image: np.ndarray, loc: float, scale: float, seed: int=None) -> np.ndarray:
+    """Apply uniform noise to given grayscale or rgb image.
+
+    Check scipy.stats.uniform for more information.
+
+    Parameters
+    ---------- 
+    image : np.ndarray
+        The image which the noise will be added. It can be gray, RGB or with multiple channels.
+    loc : float, optional
+        Loc (center) of the distribution. Default 0.0
+    scale : float, optional
+        Scale of the distribution. Default 0.1
+    seed : int, optional
+        Seed to be used while adding noise randomly. Default None.
+    
+    Raises
+    ------
+    noize.util.BadShapeException
+        If the shape is not proper.
+
+    Returns
+    -------
+    np.ndarray
+        The noise applied image. It will be in same shape with the input image.
+    """
     return __noise_with_pdf(image, stats.uniform.rvs, loc=loc, scale=scale, random_state=seed)
 
 
 def __noise_with_pdf(im_arr: np.ndarray, pdf: Callable, **kwargs) -> np.ndarray:
+    """Apply noise to given image array using pdf function that generates random noise."""
     util.check_input(im_arr)
     im_arr = im_arr/255.0
     noise = pdf(**kwargs, size=im_arr.shape)
